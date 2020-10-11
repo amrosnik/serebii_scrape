6058a0d8b6feeb579dc0e2fb12a0593360b1ebb5
amrosnik <amrosnik@gmail.com>
2020-10-11 11:23:22 -0700

    starting to clean up exception cases. Working code for data with missing form data. I have at least 24 more exceptions to go...

--- base_stats_explore.py
+++ base_stats_explore.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pokescrape as scrape
+import numpy as np
 
 pd.set_option('display.max_row', 1050)
 pd.set_option('display.max_column', 10)
@@ -59,7 +60,7 @@ simples = simple_table.columns
 bulbas = bulba_table.columns
 
 
-for name in poke_names[0:151]:
+for name in poke_names[450:500]:
     joined_table = join_simple_bulba(name)
     ## if we have Mega Evolution data, let's drop it for now.
     ## maybe I'll include it at some point, but my assumption 
@@ -71,22 +72,48 @@ for name in poke_names[0:151]:
     ## if there is, copy its simple_table data into it 
     standard_type_row = joined_table[joined_table['type_name'] == 'standard']
     if len(joined_table[joined_table[simples].isna().any(axis=1)]) > 0:
-        print("simple_table columns are NaN")
+        print("simple_table columns are NaN for ",name)
         if len(standard_type_row) > 1:
             print("WARNING! we have multiple standard type rows for whatever reason.")
         elif len(standard_type_row) == 1:
-            print("there exists a standard type row! Now let's copy standard type data for our missing type data for this special form.")
-            joined_table['name'].fillna(value=standard_type_row['name'].values[0],inplace=True)
-            joined_table['number'].fillna(value=standard_type_row['number'].values[0],inplace=True)
-            joined_table['type_name'].fillna(value=standard_type_row['type_name'].values[0],inplace=True)
-            joined_table['type'].fillna(value=standard_type_row['type'].values[0],inplace=True)
-            joined_table['generation'].fillna(value=standard_type_row['generation'].values[0],inplace=True)          
+            print("there exists a standard type row for ",name,"! Now let's copy standard type data for our missing type data for this special form.")
+            for y in simples:
+                joined_table[y].fillna(value=standard_type_row[y].values[0],inplace=True)
         elif len(standard_type_row) == 0:
             print("WARNING: We don't have a standard type row. Will need to look to some other row for filling in these data.")
+    standard_form_row = joined_table[joined_table['form_name'].str.contains('standard|normal',regex=True,na=False)]
+    if len(joined_table[joined_table[bulbas].isna().any(axis=1)]) > 0:
+        print("bulba_table columns are NaN for ",name)
+        if len(standard_form_row) > 1:
+            print("WARNING! we have multiple standard form rows for whatever reason.")
+        elif len(standard_form_row) == 1:
+            print("there exists a standard form row for ",name,"! Now let's copy standard form data for our missing form data for this special type.")
+            for x in bulbas:
+                joined_table[x].fillna(value=standard_form_row[x].values[0],inplace=True)
+        elif len(standard_form_row) == 0:
+            print("WARNING: We don't have a standard form row. Will need to look to some other row for filling in these data.")
+
+    ## EXCEPTION HANDLING
+    # Giratina (#487) has no standard form, but rather two forms (Altered and Origin). Counterintuitively, the Altered form is the more common one.
+    # so let's delete the "standard type" row with NaNs in the form entries
+    if name == "giratina":
+        joined_table = joined_table.drop(standard_type_row.index)
+    # Rotom (#479) has a LOT of forms with type differences. My two tables labeled the standard form/type differently,
+    # but they contain the same data. Dropping one of them. 
+    # also, apparently Rotom Dex can't be used as a Pokemon in battle and breeding, so let's drop that, too.
+    if name == "rotom":
+        joined_table['type_name'].replace(to_replace='^rotom',value='standard',regex=True,inplace=True)
+        joined_table = joined_table.drop(standard_form_row.index)
+        joined_table = joined_table.drop(joined_table[joined_table['type_name']=='rotom dex'].index)
+        #print(standard_type_row)
+        #print(standard_form_row)
+    ## Drop any duplicate standard/normal rows 
+
     #print(joined_table)
     big_joined_table = big_joined_table.append(joined_table,ignore_index=True)
 
-print(big_joined_table)
+print(big_joined_table.iloc[:,np.r_[0:3,5,7:10]])
+#big_joined_table.to_pickle("./joined_table_pickle.pkl")
 
 ## TODO: figure out a way to do NaN handling for cases when there is a type_name but no corresponding form_name
 ## ...there should be about 32 cases, excluding the 50 Mega Evolutions.
@@ -103,3 +130,6 @@ print(big_joined_table)
 ## Excluding the Mega forms, and counting out all the special forms 
 ## that don't have a type difference but a base stat difference, 
 ## for whatever reason there are FIVE things I can't account for.
+
+#idx = pd.IndexSlice
+#df = pd.read_pickle("./adventure_pickle_CLEAN.pkl")
