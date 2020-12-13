0693e27f30939610015845fe55f205b764d246d1
amrosnik <amrosnik@gmail.com>
2020-12-13 14:36:35 -0800

    Created new column for concatenated primary+secondary types. Added lines to save tabular data outputs. New files are lots of mini tabular outputs.

--- base_stats_crunch.py
+++ base_stats_crunch.py
@@ -166,10 +166,9 @@ def double_plot(df1,df2,descriptor,avg_profile=[False,False],labels=['df1','df2'
 # ********** ********** # 
 # ********** MESSING AROUND WITH MAIN DATASET ********** # 
 # ********** ********** # 
-best_worst = best_and_worst(df)
 #print("********* BEST AND WORST PER STAT ACROSS ALL POKEMON **********")
-#print(best_worst)
-#exit(0)
+#best_worst = best_and_worst(df)
+#best_worst.to_csv('table_outputs/best_worst_overall.csv',index=False)
 
 ## let's double-check that the Total and Average columns are correct
 df['amr_total'] = df.iloc[:, -8:-2].sum(axis=1)
@@ -191,9 +190,15 @@ if len(diff_total) == 1 and len(diff_mean) == 1:
 df['stdev'] = df.iloc[:, -8:-2].std(axis=1) 
 df['stdev'] = round(df['stdev'],1)
 #print("********* MOST AND LEAST WELL-ROUNDED ACROSS ALL POKEMON **********")
-#print(df.iloc[df['stdev'].idxmin()])
-#print(df.iloc[df['stdev'].idxmax()])
+#print(pd.DataFrame(df.iloc[df['stdev'].idxmin()]))
+#print(pd.DataFrame(df.iloc[df['stdev'].idxmax()]))
+#pd.DataFrame(df.iloc[df['stdev'].idxmin()]).to_csv('table_outputs/lowest_stdev_overall.csv',index=True)
+#pd.DataFrame(df.iloc[df['stdev'].idxmax()]).to_csv('table_outputs/highest_stdev_overall.csv',index=True)
 
+# which primary + secondary (non-NONE) combo is most common?
+df['primary+secondary'] = df['primary_type'] + " " + df['secondary_type']
+df['primary+secondary'] = df['primary+secondary'].str.replace(" NONE","")
+print("Most common primary+secondary type combo: ",df['primary+secondary'].mode())
 
 # ********** ********** # 
 # ********** TYPE BY TYPE ********** # 
@@ -206,12 +211,16 @@ for x in scrape.possible_types:
     primary_type_grouping = primary_type_grouping.reset_index()
     secondary_type_grouping = df[df['secondary_type'].str.contains(x,regex=True,na=False)]
     secondary_type_grouping = secondary_type_grouping.reset_index()
+
     ## get the type totals per each generation
     for j in range(1,9):
        generation_grp_1 = primary_type_grouping[(primary_type_grouping['generation']==j) & (primary_type_grouping['primary_type'] == x)]
        generation_grp_2 = secondary_type_grouping[(secondary_type_grouping['generation']==j) & (secondary_type_grouping['secondary_type'] == x)]
        new_row = {'type': x, 'primary': len(generation_grp_1), 'secondary': len(generation_grp_2), 'generation':j }
        simple_stats = simple_stats.append(new_row,ignore_index=True)
+
+    # TODO: create a simple_stats equivalent for dual-types? i.e., by looking at the primary+secondary column in df. 
+    ## akin to the study in http://rstudio-pubs-static.s3.amazonaws.com/67019_5d0a712031c34eea9f7d4095740da8d8.html 
 
     ## append new row for total of each type across all generations
     new_row = {'type': x, 'primary': len(primary_type_grouping), 'secondary': len(secondary_type_grouping), 'generation': 999 }
@@ -222,18 +231,23 @@ for x in scrape.possible_types:
     best_worst_2 = best_and_worst(secondary_type_grouping)
     #print(best_worst_1)
     #print(best_worst_2)
+    #best_worst_1.to_csv('table_outputs/best_worst_primary_type_'+x+'.csv',index=False)
+    #best_worst_2.to_csv('table_outputs/best_worst_secondary_type_'+x+'.csv',index=False)
 
     ## see which pokemon is closest to the average picture for its type.
     avg_poke = avg_stdev_per_type(primary_type_grouping)
     most_avg = find_most_avg(primary_type_grouping,avg_poke)
     #base_stat_plot(avg_poke,"Average profile of primary type "+x,avg_profile=True)
+    #print("Most average primary "+x+" type Pokemon:",most_avg)
+    #pd.DataFrame(most_avg).to_csv('table_outputs/most_avg_primary_type_'+x+'.csv',index=True)
     
     ## group primary+secondary groupings together: avg, closest to avg Pokemon  
     primary_and_secondary = pd.concat([primary_type_grouping, secondary_type_grouping], ignore_index=True)
     avg_poke12 = avg_stdev_per_type(primary_and_secondary)
     most_avg12 = find_most_avg(primary_and_secondary,avg_poke)
     #print(avg_poke12)
-    #print(most_avg12)
+    #print("Most average primary OR secondary "+x+" type Pokemon:",most_avg12)
+    #pd.DataFrame(most_avg12).to_csv('table_outputs/most_avg_primary-secondary_type_'+x+'.csv',index=True)
 
     #double_plot(avg_poke,avg_poke12,"Average profiles of "+x+" type",avg_profile=[True,True],labels=['primary type','primary+secondary type'])
     #base_stat_plot(avg_poke12,"Average profile of mixed type "+x,avg_profile=True)
@@ -250,6 +264,7 @@ simple_stats['secondary'] = simple_stats['secondary'].astype('int32')
 simple_stats['generation'] = simple_stats['generation'].astype('int32')
 simple_stats = simple_stats[['type','primary','secondary','generation']]
 #print(simple_stats)
+exit(0)
 
 #plot the numbers of each primary and secondary type per generation
 for gen in simple_stats['generation'].unique():
@@ -259,10 +274,10 @@ for gen in simple_stats['generation'].unique():
      # most common types per generation
      max_primary = gen_data.iloc[gen_data['primary'].idxmax()]
      max_secondary = gen_data.iloc[gen_data['secondary'].idxmax()]
-     print(gen,max_primary['type'],max_secondary['type'])
+     print("Most common primary and secondary types in gen "+str(gen)+": ",max_primary['type'],max_secondary['type'])
      min_primary = gen_data.iloc[gen_data['primary'].idxmin()]
      min_secondary = gen_data.iloc[gen_data['secondary'].idxmin()]
-     print(gen,min_primary['type'],min_secondary['type'])
+     print("Least common primary and secondary types in gen "+str(gen)+": ",min_primary['type'],min_secondary['type'])
   
      ## plot all types for a given generation
      primary_vals = gen_data[['primary']].values
@@ -308,6 +323,7 @@ for t in scrape.possible_types:
      secondary_vals = primary_data[['secondary']].values
      secondary_vals = [y for x in secondary_vals for y in x]
 
+     # TODO: normalize these values by gen total 
      generations = np.arange(1,len(primary_vals),1)
      fig, ax1 = plt.subplots()
      ax1.set_xlabel("Generation")
@@ -319,13 +335,8 @@ for t in scrape.possible_types:
      ax1.plot(generations,secondary_vals[:-1],color='g',label='secondary type',marker='.')
      ax1.legend(loc='best')
      fig.tight_layout()
-     plt.show()
+     #plt.show()
      plt.close()
-
-# TODO: mean, median, mode style questions 
-# which primary + secondary (non-NONE) combo is most common?
-# do this by making (temporary) new column that's a concatenated string of primary + secondary, where
-# NONE is not concatenated if that is the secondary type. Then, do mode on that new column.
 
 # TODO: miscellaneous questions to ask 
 # do Pokemon of a given type follow a pattern for relative magnitudes of base stats? 
